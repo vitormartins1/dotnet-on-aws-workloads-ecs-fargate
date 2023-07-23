291d41d547af729bdd7ba5c6bc65b0b1c57b6b74
Vitor Martins <vitormartins.one@gmail.com>
2023-07-23 00:11:29 -0300

    ajustes

--- WeatherSite/Pages/Index.cshtml.cs
+++ WeatherSite/Pages/Index.cshtml.cs
@@ -5,15 +5,32 @@ namespace WeatherSite.Pages;
 
 public class IndexModel : PageModel
 {
-    private readonly ILogger<IndexModel> _logger;
+    [BindProperty(SupportsGet = true)]
+    public string Location { get; set; } = "Dallas";
 
-    public IndexModel(ILogger<IndexModel> logger)
+    public string Message { get; private set; } = "";
+    public IEnumerable<WeatherForecast>? Items { get; private set; }
+
+    private readonly IConfiguration _configuration;
+    private static readonly HttpClient _httpClient = new();
+
+    public IndexModel(IConfiguration configuration)
     {
-        _logger = logger;
+        _configuration = configuration;
     }
 
-    public void OnGet()
+    public async Task OnGet()
     {
+        try
+        {
+            var requestUri = $"{_configuration["AppSettings:WeatherAPI"]}/WeatherForecast?location={Location}";
 
+            Items = await _httpClient.GetFromJsonAsync<IEnumerable<WeatherForecast>>(requestUri);
+            Message = $"{Items?.Count()} forecast records found";
+        }
+        catch (HttpRequestException ex)
+        {
+            Message = ex.Message;
+        }
     }
 }
